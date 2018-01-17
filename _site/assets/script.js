590ada098a513885987bec1a939245aa6a61b950
Cody <cody.saylor@lehighoutfitters.com>
2018-01-17 09:31:10 -0500

    change footer live chat

--- _site/assets/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var maxHeight = -1;
-  $('.grid li').each(function(){
-    maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-  });
-  $('.grid li .card').each(function() {
-    $(this).height(maxHeight);
-  });
-});
